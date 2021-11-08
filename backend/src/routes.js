4897a21894fcccc509fb9c94a29b4619fda9c502
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-08 18:32:38 -0300

    testado e funcionando corretamente o backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,29 +16,13 @@ const upload = multer(uploadConfig);
 
 
 routes.post('/sessions', SessionController.store);
-
 routes.get('/spots', SpotController.index); 
+ 
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 routes.get('/dashboard', DashboardController.show);
 
-
-
-routes.get("/testar", (request, response) => {
-    console.log("testar - @douglasabnovato.")
-    return response.json({ idade:request.query.idade });
-})
-
-routes.put("/testou/:id", (request, response) => {
-    console.log("testou id - @douglasabnovato.")
-    return response.json({ id:request.params.id });
-})
-
-routes.post("/teste", (request, response) => {
-    console.log("teste - @douglasabnovato.")
-    return response.json(request.body);
-})
  
 module.exports = routes;
