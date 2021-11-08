6c99b426b8e699c96739ea1e17e3e540b5f166bf
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-08 14:01:47 -0300

    revisando o backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,12 @@ mongoose.connect('mongodb+srv://dbAircnc:douglas1234@cluster0.yg58x.mongodb.net/
     useUnifiedTopology: true,
 })
 
+app.get("/", (request, response) => {
+    console.log("@douglasabnovato.")
+    return response.json({ message: "Hello Week Omnistack 9!" });
+})
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333); 
+app.listen(3333);  
