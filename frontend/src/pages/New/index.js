0bfb1da398b76c47b0b2def302ca0ccc1ab5de63
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-10 10:49:14 -0300

    formulário de criação de spot

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./styles.css";
+
 export default function New(){
+
+    const [company, setCompany] = useState("");
+    const [techs, setTechs] = useState("");
+    const [price, setPrice] = useState("");
+
+    function handleSubmit(){
+
+    }
+
     return (
         <>
-            <div>
-                New
-            </div>
-            <Link to="/dashboard"> 
-                <button className="btn">Retornar aos Spots</button> 
-            </Link>
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="company">EMPRESA *</label>
+                <input 
+                    id="company" 
+                    placeholder="Sua empresa incrível" 
+                    value={company} 
+                    onChange={event => setCompany(event.target.value)}
+                />
+
+                <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
+                <input 
+                    id="techs" 
+                    placeholder="Quais tecnologias usam?" 
+                    value={techs} 
+                    onChange={event => setTechs(event.target.value)}
+                />
+
+                <label htmlFor="techs">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
+                <input 
+                    id="price" 
+                    placeholder="Valor cobrado por dia" 
+                    value={price} 
+                    onChange={event => setPrice(event.target.value)}
+                />
+
+                <button type="submit" className="btn">Cadastrar</button>
+
+            </form>
         </>
     );
 }
