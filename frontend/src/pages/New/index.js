023437c0077ecd6369fd07bac32f73a761cda225
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-10 11:43:22 -0300

    enviar spot nova para o bd

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from "react"; 
+import api from "../../services/api";
 
 import camera from "../../assets/camera.svg";
 import "./styles.css";
@@ -14,15 +15,17 @@ export default function New(){
         return thumbnail ? URL.createObjectURL(thumbnail) : null
     }, [thumbnail]);
 
-    function handleSubmit(){
-
+    async function handleSubmit(){
+        
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
 
-                <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }}>
+                <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }}
+                    className={ thumbnail ? "has-thumbnail" : ""}    
+                >
                     <input type="file" onChange={event => setThumbnail(event.target.files[0])}/>
                     <img src={camera} alt="Select img" />
                 </label>
