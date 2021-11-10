c358b08da6d8ba6ecdf1833987de72a640276500
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-10 09:44:05 -0300

    páginas dashboard, login, new

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState} from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
 import "./styles.css";
 
@@ -33,6 +34,9 @@ export default function Dashboard(){
                     </li>
                 ))}
             </ul>
+            <Link to="/new">
+                <button className="btn">Cadastrar novo Spot</button> 
+            </Link>
         </>
     );
 }
--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function New(){
     return (
-        <div>New</div>
+        <>
+            <div>
+                New
+            </div>
+            <Link to="/dashboard"> 
+                <button className="btn">Retornar aos Spots</button> 
+            </Link>
+        </>
     );
 }
